089fca89d7a65c07b9f341ee4e5c6addc7d2f9d3
David Freitas <dvdfreitas@gmail.com>
2024-08-02 14:51:55 +0100

    240802 14:51

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -8,9 +8,9 @@
                             <img src="{{ $user->profile_photo_url }}" alt="{{ $user->name }}" class="rounded-full h-36 w-36 object-cover m-auto my-2">                        
                             <div class="text-xl font-semibold text-center">{{ $user->name }}</div>
                             @if ($user->runner == "guide")
-                                <img src="/images/icons/guide.svg" alt="Guia" class="h-8 w-8 m-auto my-2">
+                                <img src="{{ asset('storage/images/icons/guide.svg') }}" alt="Guia" class="h-8 w-8 m-auto my-2">
                             @elseif ($user->runner == "athlete")
-                                <img src="/images/icons/athlete.svg" alt="Atleta" class="h-8 w-8 m-auto my-2">
+                                <img src="{{ asset('storage/images/icons/athlete.svg') }}" alt="Atleta" class="h-8 w-8 m-auto my-2">
                             @endif                            
                         </div>            
                     </a>
